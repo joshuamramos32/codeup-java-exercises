6f65676504a4e5f75ef25cb9671cf3ec148c66dd
Joshua Ramos <joshua.m.ramos32@gmail.com>
2023-01-02 11:53:22 -0600

    adding syntax data types and variables exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,37 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.print("Hello, World!");
+        //System.out.println("Hello, World!");
+        //System.out.print("Hello, World!");
+        int myFavoriteNumber =7;
+        String myString = "My favorite number is";
+        double myNumber = 3.14;
+        //String myString = 'c';
+        //String myString =3.14159;
+        System.out.println(myString);
+        System.out.println(myFavoriteNumber);
+        System.out.print(myNumber);
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) "three";
+//        int x = 4;
+//        x += 5;
+//        System.out.print(x);
+//        int x = 3;
+//        int y = 4;
+//        y *= 3;
+//        System.out.println(y);
+        int x = 10;
+        int y = 2;
+        x /= y;
+        y -= x;
+        System.out.println();
+        System.out.println(x);
+        System.out.println(y);
     }
 }

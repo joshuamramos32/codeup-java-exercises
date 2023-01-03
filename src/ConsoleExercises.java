5f79bdb3bf7a93bf03b1bb5f4835a7bf408b0fd0
Joshua Ramos <joshua.m.ramos32@gmail.com>
2023-01-03 10:27:29 -0600

    adding console exercises updates and controlflowercices

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -2,31 +2,43 @@ import java.net.SocketOption;
 import java.util.Scanner;
 public class ConsoleExercises {
     public static void main(String[] args) {
+        // 1.Copy this code into your main method:
 //        double pi = 3.14159;
+        // Write some Java code that uses the variable pi to output the following:
 //        System.out.format("The value of pi is approximately %.2f." , pi);
         Scanner scanner = new Scanner(System.in);
+//          1. Prompt a user to enter a integer and store that value in an            int variable using the nextInt method.
 
 //        System.out.print("How old are you? ");
 //        int userInput = scanner.nextInt();
 //        System.out.println("You entered: --> \"" + userInput + "\" <--");
+
+        //2. Prompt a user to enter 3 words, and store each of them in a separate variable. Then, display them back in the console, each on a newline.
 //        System.out.print("Please enter three words?");
 //        String firstWord = scanner.next();
 //        String secondWord = scanner.next();
 //        String thirdWord = scanner.next();
-//
+
 //       System.out.println(firstWord);
 //       System.out.println(secondWord);
 //       System.out.println(thirdWord);
 
+        //3.Prompt a user to enter a sentence, then store that sentence in a String variable using the next method. Then, display that sentence back to the user.
+
 //        System.out.print("Please enter a sentence?");
 //        String userSentence = scanner.nextLine();
 //        System.out.println(userSentence);
+
+        //1.Prompt the user to enter values of length and width of a classroom at Codeup (or your room if virtual).
 
         System.out.print("What is the length of your room?");
         int length = scanner.nextInt();
         System.out.print("What is the width of your room?");
         int width = scanner.nextInt();
         int perimeter = length * width;
+
+        //2.Display the area and perimeter of that classroom.
+
         System.out.println("The area of your room is " + perimeter*2);
         System.out.println("The perimeter of your room is " + perimeter);
 
---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,53 @@
+public class ControlFlowExercises {
+    public static void main(String[] args) {
+//      1a.  While
+//
+//        Create an integer variable i with a value of 5.
+//        Create a while loop that runs so long as i is less than or equal to 15
+//        Each loop iteration, output the current value of i, then increment i by one.
+//        Your output should look like this:
+//
+//        5 6 7 8 9 10 11 12 13 14 15
+//        int i =5;
+//        while (i <= 15) {
+//            System.out.println(+ i);
+//            i++;}
+//      1.b  Do While
+//
+//        Create a do-while loop that will count by 2's starting with 0 and ending at 100. Follow each number with a new line.
+//        Alter your loop to count backwards by 5's from 100 to -10.
+//        Create a do-while loop that starts at 2, and displays the number squared on each line while the number is less than 1,000,000. Output should equal:
+//        int i = 1;
+//        do{
+//            System.out.println(i*2);
+//            i++;
+//        }while (i <= 50);
+
+//        int i = 20;
+//        do{
+//            System.out.println(i*5);
+//            i--;
+//        }while (i >= -2);
+
+//        long i = 2;
+//        do{
+//            System.out.println(i);
+//            i*= i;
+//        }while (i < 1000000);
+//       1.c For
+//
+//        Refactor the previous two exercises to use a for loop instead.
+//        for(int i = 5; i <= 15; i++ ){
+//        System.out.println(+i);
+//        }
+//        for(int i = 1; i <=50; i++){
+//            System.out.println(i*2);
+//        }
+//        for(int i = 20; i>= -2;i--){
+//            System.out.println(i*5);
+//        }
+//        for(long i = 2; i<1000000; i*=i){
+//            System.out.println(i);
+        }
+    }
+}

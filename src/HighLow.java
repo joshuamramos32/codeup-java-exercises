5bcea671bc7bb4db8b63695221eeae525c361336
Joshua Ramos <joshua.m.ramos32@gmail.com>
2023-01-06 10:10:19 -0600

    adding high low completed

--- src/HighLow.java
+++ src/HighLow.java
@@ -1,2 +1,27 @@
+
+import java.util.Random;
+import java.util.Scanner;
+
 public class HighLow {
+    public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+        Random random = new Random();
+        int randomNum = random.nextInt(100) +1;
+//        System.out.println(randomNum);
+        boolean confirm = true;
+        do {
+        System.out.println("Pick a number between 1-100?");
+        int userNum = scanner.nextInt();
+//        Random random = new Random();
+//        int randomNum = random.nextInt(100) +1;
+        if(randomNum == userNum){
+            confirm = false;
+            System.out.println("Good Guess");
+        }else if (userNum < randomNum){
+            System.out.println("higher");
+        } else{
+            System.out.println("lower");
+        }
+        }while(confirm);
+    }
 }

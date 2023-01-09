97fe50424ac9696f3b635f46417c3f603fad7509
Joshua Ramos <joshua.m.ramos32@gmail.com>
2023-01-09 16:57:05 -0600

    adding nouns

---
+++ src/ServerNameGenerator.java
@@ -0,0 +1,19 @@
+public class ServerNameGenerator {
+    public static void main(String[] args) {
+        String[] adjectives = new String[10];
+// wax sock hate cherry lumber eggs agreement word beef condition
+
+       String[] nouns = new String[10];
+       nouns[0] = "Wax";
+       nouns[1] = "Sock";
+       nouns[2] = "Hate";
+       nouns[3] = "Cherry";
+       nouns[4] = "Lumber";
+       nouns[5] = "Eggs";
+       nouns[6] = "Agreement";
+       nouns[7] = "Word";
+       nouns[8] = "Beef";
+       nouns[9] = "Condition";
+
+    }
+}
